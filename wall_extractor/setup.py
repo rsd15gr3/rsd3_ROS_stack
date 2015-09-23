226244a8fb16510d45277773fc68d62f9c624898
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2015-09-23 14:32:52 +0200

    Rearrange directory structure

--- wall_extractor/setup.py
+++ wall_extractor/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    scripts=['perception/wall_extractor_node.py'])
+    scripts=['src/wall_extractor_node.py'])
 
 setup(**d)
