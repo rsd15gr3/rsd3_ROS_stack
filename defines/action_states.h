d706a4ad888ed9a7d813d0768acf5a65ad612bce
MortenGundesen <mogun11@student.sdu.dk>
2015-10-02 13:23:44 +0200

    Made changes to the defines

--- defines/action_states.h
+++ defines/action_states.h
@@ -1,10 +1,20 @@
 //Action state definitions /published to mission/action_state
-#define TEST        0
-#define IDLE        1
-#define CHARGE      2
-#define BACKTRACK   3
-#define MANUAL      4
-#define BRIDGE      5
-#define LINE        6
-#define TIPPER      7
-#define GPS         8
+//Control actions(CTR = Control)
+#define CTR_TEST    	0
+#define CTR_IDLE        1
+#define CTR_BACKTRACK   2
+#define CTR_MANUAL	3
+//Box area
+#define BOX_CHARGE      4
+#define BOX_BRICK	5
+#define BOX_DOOR 	6
+//GPS area
+#define GPS_DOOR	7
+#define GPS_LINE	8
+//#define GPS_BRIDGE
+//Line area
+#define LINE_GPS	9
+#define LINE_TIPPER	10
+//Tipper area
+#define TIPPER_UP      	11
+#define TIPPER_DOWN    	12
