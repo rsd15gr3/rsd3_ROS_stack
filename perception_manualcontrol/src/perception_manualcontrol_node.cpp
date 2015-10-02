c4ca260d680039d7853b09a58394524a52c1373a
Stig <sttur14@student.sdu.dk>
2015-10-02 16:09:29 +0200

    cleaned code a bit

--- perception_manualcontrol/src/perception_manualcontrol_node.cpp
+++ perception_manualcontrol/src/perception_manualcontrol_node.cpp
@@ -1,38 +1,42 @@
 #include<ros/ros.h>
-#include<msgs/BoolStamped.h>
+#include <msgs/BoolStamped.h>
+#include <msgs/IntStamped.h>
 #include <std_msgs/String.h>
+#include <stdio.h>
+
 
 using namespace std;
 using namespace ros;
+
 int loopRate = 5; //publish freq
-ros::Publisher pub;
+ros::Publisher manual_control_verify_publisher;
+
 
 void ui_str_control_Callback(const std_msgs::String::ConstPtr& msg)
 {
     std_msgs::String _msg = *msg;
-    msgs::BoolStamped pubmsg;
+    cout << _msg.data << endl;
     if(_msg.data == "mr_mode_manual")
     {
-            pubmsg.data = true;
-            cout << "control set to manual" << endl;
+        msgs::BoolStamped pubmsg;
+        pubmsg.data = true;
+        pubmsg.header.stamp = ros::Time::now();
+        manual_control_verify_publisher.publish(pubmsg);
     }
     else if(_msg.data == "mr_mode_auto")
     {
-            pubmsg.data = false;
-            cout << "control set to auto" << endl;
+        msgs::BoolStamped pubmsg;
+        pubmsg.data = false;
+        pubmsg.header.stamp = ros::Time::now();
+        manual_control_verify_publisher.publish(pubmsg);
     }
-    else
-        return;
-
-    pubmsg.header.stamp = ros::Time::now();
-    pub.publish(pubmsg);
 }
 
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "perception_manualcontrol_node");
     NodeHandle n;
-    pub = n.advertise<msgs::BoolStamped>("perception/manualcontrol_verify",1);
+    manual_control_verify_publisher = n.advertise<msgs::BoolStamped>("perception/manualcontrol_verify",1);
     ros::Subscriber sub = n.subscribe("ui_str_control", 1, ui_str_control_Callback);
     ros::spin();
     return 0;
