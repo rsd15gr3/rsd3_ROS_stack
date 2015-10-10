5a501a5ec3711be802d6977e53b6a5ba5834c049
Birkehoj <anders.birkehoj@gmail.com>
2015-10-10 17:03:23 +0200

    Made action_states.h part of the mission package for easy include Now the action_states can be included like other .h files in catkin packages. The relative path to the file is avoided

---
+++ Mission-code/include/mission/action_states.h
@@ -0,0 +1,20 @@
+//Action state definitions /published to mission/action_state
+//Control actions(CTR = Control)
+#define CTR_TEST    	0
+#define CTR_IDLE        1
+#define CTR_BACKTRACK   2
+#define CTR_MANUAL	3
+//Box area
+#define BOX_CHARGE      4
+#define BOX_BRICK	5
+#define BOX_DOOR 	6
+//GPS area
+#define GPS_DOOR	7
+#define GPS_LINE	8
+//#define GPS_BRIDGE
+//Line area
+#define LINE_GPS	9
+#define LINE_TIPPER	10
+//Tipper area
+#define TIPPER_UP      	11
+#define TIPPER_DOWN    	12
