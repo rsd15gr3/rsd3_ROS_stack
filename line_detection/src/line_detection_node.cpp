eed4940f834491ec9b93fdf50726ab3adbbddb8d
Birkehoj <anders.birkehoj@gmail.com>
2015-10-14 13:20:30 +0200

    Simple line detection Using horizontal scan lines

--- line_detection/src/line_detection_node.cpp
+++ line_detection/src/line_detection_node.cpp
@@ -11,34 +11,44 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <msgs/BoolStamped.h>
+#include <line_detection/line.h>
 
 using namespace cv;
 using std::vector;
 using std::string;
 
+namespace line_types
+{
+    enum Line_type
+    {
+        NO_LINE,
+        LINE,
+        CROSS,
+    };
+}
+typedef line_types::Line_type Line_type;
+
 void imageCb(const sensor_msgs::ImageConstPtr& msg);
 void camInfoCb(const sensor_msgs::CameraInfoConstPtr& info_msg);
 inline bool advancedEdgeDetection(const Mat &image, Mat &edges);
+Line_type scanline(Mat lineImage, int line, int &mid);
+
 image_transport::Publisher image_pub;
+ros::Publisher line_pub;
+int loop_rate = 20;
+bool verification = false; /* Verify is set true when is likley that a "preception" is valid, */
+unsigned threshold_gray = 120;
 
-int loopRate = 20;
-bool verify = false; /* Verify is set true when is likley that a "preception" is valid, */
+vector<double> cam_mat {1232.801045, 0, 640, 0, 1240.089791, 360, 0, 0, 1};
 
-const double radPerDeg = CV_PI/180;
-// Canny
-const unsigned low_threshold = 100;
-const unsigned low_to_high_ration = 4; // 2 or 3
-const unsigned kernel_size = 3;
-// Advanced edge detection
-const unsigned MIN_COUNTOUR_AREA = 20000; // PIXELS
-const unsigned SECTION_SIZE = 80; // PIXELS
-// Probablistic Hough
-const unsigned rho = 2;
-const unsigned theta = 2;
-const unsigned min_no_of_intersections = 70;
-const unsigned min_line_length = 150;
-const unsigned max_line_gap = 20;
-vector<double> cam_mat {1232.801045, 0, 643.489543, 0, 1240.089791, 375.302479, 0, 0, 1};
+const double f_x = 1232.801045;
+const double f_y = 1240.089791;
+const double c_x = 640;
+const double c_y = 360;
+const Eigen::Vector2d cam_center(c_x, c_y);
+
+const unsigned LINE_WIDTH = 60;
+const unsigned CROSS_WIDTH = 300;
 
 int main(int argc, char** argv)
 {
@@ -50,18 +60,18 @@ int main(int argc, char** argv)
     image_sub = it.subscribe("/usb_cam/image_raw", 1, &imageCb);
     image_pub = it.advertise("/perception/floor_lines", 1);
 
-    ros::Publisher pub = n.advertise<msgs::BoolStamped>("perception/example_verify_pub",1);
-    ros::Rate loop_rate(loopRate);
+    ros::Publisher verify_pub = n.advertise<msgs::BoolStamped>("perception/line_verify",1);
+    line_pub = n.advertise<line_detection::line>("perception/line",1);
+    ros::Rate loop_rate(loop_rate);
 
     while (ros::ok()) //Code loop
     {
-        verify = !verify;
 
         msgs::BoolStamped msg;
         msg.header.stamp = ros::Time::now();
-        msg.data = verify;
+        msg.data = verification;
 
-        pub.publish(msg);
+        verify_pub.publish(msg);
 
         loop_rate.sleep(); //sleep to match pub freq for testing
         ros::spinOnce();
@@ -82,110 +92,121 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
-    Mat edges;
-    // Canny(cv_ptr->image, edges, low_threshold, low_threshold*low_to_high_ration, kernel_size);
-    bool valid_line = advancedEdgeDetection(cv_ptr->image, edges);
-    imshow("edges of line", edges);
-    cv::waitKey(5);
-    if(valid_line) {
-        vector<Vec4i> lines;
-        HoughLinesP(edges, lines, rho, theta*radPerDeg, min_no_of_intersections, min_line_length, max_line_gap);
-        // Run through lines and parametrize
-        vector<Eigen::ParametrizedLine<double, 2> > param_lines(lines.size());
-        ROS_INFO("Found %i lines",(int)lines.size());
-        for( size_t i = 0; i < lines.size(); i++ )
-        {
-          Vec4i l = lines[i];
-          line( cv_ptr->image, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA); // plot lines
-          // convert to line to parametrized line
-          Eigen::Vector2d pr0(l[0], l[1]); Eigen::Vector2d pr1(l[2], l[3]);
-          param_lines[i] = Eigen::ParametrizedLine<double, 2>::Through(pr0, pr1);
-          //ROS_INFO("Origin = %f", param_lines[i].origin()[0]);
-        }
-        /*
-        // combine lines
-        // atan2(a/1)
-        // calculate error from center http://eigen.tuxfamily.org/dox/classEigen_1_1ParametrizedLine.html
-        l.origin();
-        double dist = l.distance()
-        */
-        image_pub.publish(cv_ptr->toImageMsg());
-        verify = (lines.size() >= 2); // TODO: improve verify
+    Mat lineImage;
+    cvtColor(cv_ptr->image,lineImage,CV_BGR2GRAY);
+    int midOfTop;
+    int top_scanline = 0;
+    Line_type type_top = scanline(lineImage, top_scanline, midOfTop);
+
+    //search until a line without intersection is found
+    while(type_top == line_types::CROSS)
+    {
+        top_scanline += 50;
+        type_top = scanline(lineImage, top_scanline, midOfTop);
     }
-    else {
+
+    int midOfBot;
+    int bot_scanline = lineImage.rows-1;
+    Line_type type_bot = scanline(lineImage, bot_scanline, midOfBot);
+    //search until a line without intersection is found
+    while(type_bot == line_types::CROSS)
+    {
+        bot_scanline -= 50;
+        type_bot = scanline(lineImage, bot_scanline, midOfBot);
+    }
+
+    if(type_top == line_types::LINE && type_bot == line_types::LINE)
+    {
+        verification = true;
+    } else {
         ROS_INFO("No valid edge region of line found");
     }
+
+    cv::line(lineImage, Point(midOfTop, top_scanline), Point(midOfBot, bot_scanline), 255, 2);
+    Eigen::Vector2d pr0(midOfTop, top_scanline); Eigen::Vector2d pr1(midOfBot, bot_scanline);
+    pr0 -= cam_center;
+    pr1 -= cam_center;
+    pr0[0] /= f_x; pr0[1] /= f_y;
+    pr1[0] /= f_x; pr1[1] /= f_y;
+
+   Eigen::ParametrizedLine<double, 2> param_line = Eigen::ParametrizedLine<double, 2>::Through(pr0, pr1);
+
+    //Calculate line
+    Eigen::Vector2d gradient = param_line.direction();
+    double angle = atan2(gradient[1], gradient[0]);
+
+    Eigen::NumTraits<Scalar>::Real offset_real = param_line.distance(cam_center);
+
+    double offset = offset_real[0];
+    offset *=  (midOfTop > 0 ? 1 : -1);
+    cv::circle(lineImage, Point(lineImage.cols/2+offset,0), 2, 255, 2);
+    imshow("slider", lineImage);
+    line_detection::line line_msg;
+    line_msg.header.stamp = ros::Time::now();
+    line_msg.header.frame_id = ""; // TODO:
+    line_msg.angle = angle;
+    line_msg.offset = offset;
+    line_pub.publish(line_msg);
+    cv::waitKey(5);
 }
 
-inline void discrete_threshold(const Mat &image, Mat &binaryIm, int thresholdGray, int light)
+
+Line_type scanline(Mat lineImage, int line, int &mid)
 {
-    //convert to gray
-    Mat gray;
-    try {
-    cvtColor(image, gray, CV_BGR2GRAY);    
-    }
-    catch (cv::Exception e) {
-        ROS_ERROR("cv exception: %s", e.what());
-    }
-    int splitW = image.cols/SECTION_SIZE;
-    int splitH = image.rows/SECTION_SIZE;
-    double min, max;
-    double adjusted_threshold;
+    unsigned start, width, total_width = 0;
+    mid = -1;
+    bool counting = false;
+    Line_type type = Line_type::NO_LINE;
 
-    for(int i=0; i<splitH*splitW ; i++)
+    //scanline 1
+    uchar* p;
+    p = lineImage.ptr<uchar>(line);
+    for(unsigned x=0; x<lineImage.cols; x++)
     {
-        Mat part (gray, Rect(SECTION_SIZE*(i%splitW), SECTION_SIZE*(i/splitW), SECTION_SIZE, SECTION_SIZE) );
-
-        cv::minMaxLoc(part, &min, &max);
-        adjusted_threshold = max*light/255 + thresholdGray;
-        //threshold(part, part, adjusted_threshold, 255, 1);
-        inRange(part, 0, adjusted_threshold, part);
-
-        Rect roi(SECTION_SIZE*(i%splitW), SECTION_SIZE*(i/splitW), SECTION_SIZE, SECTION_SIZE);
-        part.copyTo( binaryIm(roi) );
-    }
-}
-
-inline bool get_largest_contour(Mat &binaryIm, unsigned min_contour_area)
-{
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    findContours( binaryIm, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-    double largest_area=0;
-    int contour_index=0;
-
-    for( int i = 0; i< contours.size(); i++ )
-    {
-        double area = contourArea(contours[i]);
-        if(area > largest_area)
+        //threshold to seek for line
+        if(p[x] < threshold_gray)
         {
-            largest_area = area;
-            contour_index = i;
+            //count the width of the found dark scan
+            if(counting == false)
+            {
+                start = x;
+                width = 1;
+                counting = true;
+            }
+            else
+            {
+                width++;
+                total_width++;
+            }
+        }
+        //depending on length define what is found
+        else if(counting == true)
+        {
+            counting = false;
+            if(width > LINE_WIDTH) //this might happen twice and give a bug
+            {
+                type = Line_type::LINE;
+                mid = start+width/2;
+
+            }
+            if(total_width > CROSS_WIDTH)
+            {
+                type = Line_type::CROSS;
+                mid = start+width/2;
+            }
         }
     }
-    binaryIm = Mat::zeros( binaryIm.size(), CV_8UC1 );
 
-    bool big_area = (largest_area > min_contour_area);
-    if(big_area) {
-        drawContours( binaryIm, contours, contour_index, Scalar( 255, 255, 255 ), 2, 8, hierarchy, 0, Point() );
-        // remove white edge at edge of image
-        const int edge_remove_size = 5;
-        binaryIm(Range::all(), Range(0,edge_remove_size)) = Scalar(0);
-        binaryIm(Range::all(), Range(binaryIm.cols-edge_remove_size,binaryIm.cols) ) = Scalar(0);
-        binaryIm(Range(0,edge_remove_size), Range::all()) = Scalar(0);
-        binaryIm(Range(binaryIm.rows-edge_remove_size,binaryIm.rows), Range::all()) = Scalar(0);
-    } // Else return black area
-    return big_area;
-}
+    if(width > LINE_WIDTH) //this might happen twice and give a bug
+    {
+        type = Line_type::LINE;
+        mid = start+width/2;
 
-inline bool advancedEdgeDetection(const Mat &image, Mat &edges)
-{
-    const unsigned thresholdGray = 70;
-    const unsigned light = 70;
-    edges = Mat::zeros( image.size(), CV_8UC1 ); //gray
-    discrete_threshold(image, edges, thresholdGray, light);
-    bool line_found = get_largest_contour(edges, MIN_COUNTOUR_AREA);
-    return line_found;
+    }
+    if(total_width > CROSS_WIDTH)
+    {
+        type = Line_type::CROSS;
+        //mid = start+width/2;
+    }
+    return type;
 }
