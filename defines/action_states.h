eb7da815d0a9d2c4e18e0aae8894adbd1ca01d43
MortenGundesen <mogun11@student.sdu.dk>
2015-10-14 10:24:40 +0200

    added comment of verify name to define

--- defines/action_states.h
+++ defines/action_states.h
@@ -1,20 +1,20 @@
 //Action state definitions /published to mission/action_state
-//Control actions(CTR = Control)
+//Control actions(CTR = Control) perception/manualcontrol_verify
 #define CTR_TEST    	0
 #define CTR_IDLE        1
 #define CTR_BACKTRACK   2
 #define CTR_MANUAL	3
-//Box area
+//Box area perception/box_verify
 #define BOX_CHARGE      4
 #define BOX_BRICK	5
 #define BOX_DOOR 	6
-//GPS area
+//GPS area perception/gps_verify
 #define GPS_DOOR	7
 #define GPS_LINE	8
 //#define GPS_BRIDGE
-//Line area
+//Line area perception/line_verify
 #define LINE_GPS	9
 #define LINE_TIPPER	10
-//Tipper area
+//Tipper area perception/tipper_verify
 #define TIPPER_UP      	11
 #define TIPPER_DOWN    	12
