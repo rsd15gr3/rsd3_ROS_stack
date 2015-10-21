f7c3d081549e75f928fd5c62e1ebec32d350a412
Birkehoj <anders.birkehoj@gmail.com>
2015-10-21 10:21:45 +0200

    Line detection bug fixed

--- line_detection/src/line_detection_node.cpp
+++ line_detection/src/line_detection_node.cpp
@@ -43,7 +43,7 @@ bool verification = false; /* Verify is set true when is likley that a "precepti
 int threshold_gray = 120;
 int line_width = 60;
 int cross_width = 300;
-
+string camera_frame_id;
 // TODO: read in camera matrix from file or camera
 vector<double> cam_mat {1232.801045, 0, 640, 0, 1240.089791, 360, 0, 0, 1};
 const double f_x = 1232.801045;
@@ -64,15 +64,16 @@ int main(int argc, char** argv)
     string verification_topic;
     n.param<string>("verification_topic", verification_topic, "line_verify");
     string line_topic;
-    n.param<string>("line_topic", line_topic, "line");
+    n.param<string>("line_topic", line_topic, "line");    
+    n.param<string>("camera_frame_id", camera_frame_id, "camera_link");
     bool show_line_enb;
     n.param<bool>("show_lines", show_line_enb, false);
     image_transport::ImageTransport it(n);
     image_transport::Subscriber image_sub;
     image_sub = it.subscribe("/image_raw", 1, &imageCb);
-    //verify_pub = n.advertise<msgs::BoolStamped>(verification_topic,1);
+    verify_pub = n.advertise<msgs::BoolStamped>(verification_topic,1);
     line_pub = n.advertise<line_detection::line>(line_topic,1);
-    // ros::Timer timeVerify = n.createTimer(ros::Duration(1.0 / verify_pub_rate), verifyPubCb);
+    ros::Timer timeVerify = n.createTimer(ros::Duration(1.0 / verify_pub_rate), verifyPubCb);
     ros::spin();
     return 0;
 }
@@ -100,16 +101,24 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
     }
 
     //search from top until a line without intersection is found or center of image
-    int midOfTop, top_scanline;
-    Line_type type_top;
-    for(top_scanline=0; top_scanline < cv_ptr->image.rows/2 && type_top == line_types::CROSS; top_scanline += 50)
+    int midOfTop, top_scanline=0;
+    Line_type type_top = scanline(cv_ptr->image, top_scanline, midOfTop);
+    while(type_top == line_types::CROSS && top_scanline < cv_ptr->image.rows)
+    {
+        top_scanline += 50;
+        ROS_INFO("%i",top_scanline);
         type_top = scanline(cv_ptr->image, top_scanline, midOfTop);
+    }
 
     //search from bottum until a line without intersection is found or center of image
-    int midOfBot, bot_scanline;
-    Line_type type_bot;
-    for(bot_scanline=cv_ptr->image.rows-1; bot_scanline >= cv_ptr->image.rows/2 && type_bot == line_types::CROSS; top_scanline -= 50)
+    int midOfBot, bot_scanline = cv_ptr->image.rows-1;
+    Line_type type_bot = scanline(cv_ptr->image, bot_scanline, midOfBot);
+    //search until a line without intersection is found
+    while(type_bot == line_types::CROSS && bot_scanline >= 0)
+    {
+        bot_scanline -= 50;
         type_bot = scanline(cv_ptr->image, bot_scanline, midOfBot);
+    }
 
     // Set verification flag
     if(type_top == line_types::LINE && type_bot == line_types::LINE)
@@ -157,66 +166,10 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg)
     // Debug plot ( TODO: move debug plot to other node )
     Mat line_img;
     threshold(cv_ptr->image, line_img,threshold_gray,255,0);
-    //cv::line(line_img, Point(midOfTop, top_scanline), Point(midOfBot, bot_scanline), 150, 2);
-    //cv::circle(line_img, Point(cv_ptr->image.cols/2+offset,0), 2, 100, 2);
+    cv::line(line_img, Point(midOfTop, top_scanline), Point(midOfBot, bot_scanline), 150, 2);
+    cv::circle(line_img, Point(cv_ptr->image.cols/2+offset,0), 2, 100, 2);
     imshow("lines", line_img);
-    cv::waitKey(0.001);
-    /*
-    Errors in while loops when no line exists
-    Line_type type_top = scanline(cv_ptr->image, top_scanline, midOfTop);
-
-    while(type_top == line_types::CROSS)
-    {
-        top_scanline += 50;
-        ROS_INFO("%i",top_scanline);
-        type_top = scanline(cv_ptr->image, top_scanline, midOfTop);
-    }
-    int midOfBot;
-    int bot_scanline = cv_ptr->image.rows-1;
-    Line_type type_bot = scanline(cv_ptr->image, bot_scanline, midOfBot);
-    //search until a line without intersection is found
-    while(type_bot == line_types::CROSS)
-    {
-        bot_scanline -= 50;
-        type_bot = scanline(cv_ptr->image, bot_scanline, midOfBot);
-    }
-
-    if(type_top == line_types::LINE && type_bot == line_types::LINE)
-    {
-        verification = true;
-    } else {
-        ROS_INFO("No valid edge region of line found");
-    }
-
-    //cv::line(cv_ptr->image, Point(midOfTop, top_scanline), Point(midOfBot, bot_scanline), 255, 2);
-    Eigen::Vector2d pr0(midOfTop, top_scanline); Eigen::Vector2d pr1(midOfBot, bot_scanline);
-    pr0 -= cam_center;
-    pr1 -= cam_center;
-    pr0[0] /= f_x; pr0[1] /= f_y;
-    pr1[0] /= f_x; pr1[1] /= f_y;
-
-   Eigen::ParametrizedLine<double, 2> param_line = Eigen::ParametrizedLine<double, 2>::Through(pr0, pr1);
-
-    //Calculate line
-    Eigen::Vector2d gradient = param_line.direction();
-    double angle = atan2(gradient[1], gradient[0]) - CV_PI/2;
-
-    Eigen::NumTraits<Scalar>::Real offset_real = param_line.distance(Eigen::Vector2d(0,0));
-
-    double offset = offset_real[0];
-    offset *=  (pr0[0] > 0 ? 1 : -1);
-
-    //cv::circle(cv_ptr->image, Point(cv_ptr->image.cols/2+offset,0), 2, 255, 2);
-
-    line_detection::line line_msg;
-    line_msg.header.stamp = ros::Time::now();
-    line_msg.header.frame_id = ""; // TODO:
-    line_msg.angle = angle;
-    line_msg.offset = offset;
-    line_pub.publish(line_msg);
-    //imshow("slider", cv_ptr->image);
-    //cv::waitKey(5);
-    */
+    cv::waitKey(1);
 }
 
 
