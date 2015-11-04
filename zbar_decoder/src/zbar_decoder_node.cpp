d65b2eb2df356e845c3341fd791accb307f0005c
Birkehoj <anders.birkehoj@gmail.com>
2015-11-04 21:46:00 +0100

    zbar_decoder publish when qr is out of image with debounce

--- zbar_decoder/src/zbar_decoder_node.cpp
+++ zbar_decoder/src/zbar_decoder_node.cpp
@@ -16,15 +16,41 @@ using namespace std;
 using namespace zbar;
 using namespace cv;
 
-#define ROSCONSOLE_MIN_SEVERITY ROSCONSOLE_SEVERITY_ERROR //ROSCONSOLE_SEVERITY_INFO, ROSCONSOLE_SEVERITY_DEBUG
+#define ROSCONSOLE_MIN_SEVERITY ROSCONSOLE_SEVERITY_DEBUG //ROSCONSOLE_SEVERITY_INFO, ROSCONSOLE_SEVERITY_DEBUG
 
 sensor_msgs::Image rawImg;
 const string node_name("zbar_decoder_node");
+const string no_tag_id("?");
 ImageScanner scanner;
-string previous_tag_value;
+string tag_value(no_tag_id);
+bool prev_tag_found;
 // change values to match workcells code
-vector<string> knownCodes;// {"http://www.youtube.com/watch?v=S5KafuOcyn4", "http://www.qrstuff.com", "IT'S BACK ON DVD SEPTEMBER 10th" };
 ros::Publisher pub;
+vector<string> knownCodes;//
+
+unsigned debounce_size = 3;
+class BoolDebouncer
+{
+public:
+  BoolDebouncer(unsigned size) : size(size), equals_count(0) {}
+  bool update(bool val) {
+    if(val == prev_val) {
+      if(++equals_count > size) {
+        equals_count = size; // to avoid overflow
+        debuounced_val = val;
+      }
+    }
+    else {
+      equals_count = 0;
+    }
+    prev_val = val;
+    return debuounced_val;
+  }
+private:
+  unsigned size, equals_count;
+  bool prev_val, debuounced_val;
+};
+BoolDebouncer debouncer(debounce_size);
 
 void camCallback(const sensor_msgs::Image::ConstPtr& img )
 {
@@ -39,7 +65,6 @@ void camCallback(const sensor_msgs::Image::ConstPtr& img )
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
-
   // extreact qr code
   uchar *raw = (uchar *)cv_ptr->image.data;
   unsigned width = cv_ptr->image.cols;
@@ -48,51 +73,29 @@ void camCallback(const sensor_msgs::Image::ConstPtr& img )
   Image image(width, height, "Y800", raw, width * height);
   // scan the image for barcodes
   int n = scanner.scan(image);
-  if(n > 0) // qr found?
-  {
-    string tagValue("Unknown");
-    bool knownCodeNotFound = true;
-    // find first known tag
-    for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end() && knownCodeNotFound; ++symbol)
-    {
-      for(int i=0; i<knownCodes.size(); i++)
-      {
-        if(symbol->get_data() == knownCodes[i])
-        {
-          tagValue = symbol->get_data();
-          knownCodeNotFound = false;
-          break;
-        }
-      }
-    }
-    if(knownCodeNotFound)
-    {
-      ROS_DEBUG_NAMED(node_name,"No known code found");
-    }
-    else
-    {
-      ROS_DEBUG(tagValue.c_str());
-      if(tagValue != previous_tag_value)
-      {
-        previous_tag_value = tagValue;
-        msgs::StringStamped msgs;
-        msgs.header.stamp = ros::Time::now();
-        msgs.header.frame_id = "camera_link";
-        msgs.data = tagValue;
-        pub.publish(msgs);
-      }
-    }
-  }
-  else if(n < 0)
+  if(n < 0)
   {
     ROS_ERROR_NAMED(node_name,"Error decoding image with Image scan");
     return;
   }
-  else // else n==0 -> no known kode found
+  bool current_tag_found = n > 0;
+  bool tag_found = debouncer.update(current_tag_found);
+  if(tag_found) // qr found?
   {
-    ROS_DEBUG_NAMED(node_name,"No qr code found");
-
+    ROS_DEBUG("Tag found");
+    // first found tag is used
+    tag_value = image.symbol_begin()->get_data();
   }
+  else if(prev_tag_found) // else n==0 -> no code found
+  {
+    ROS_DEBUG("Tag moved out of camera view");
+    msgs::StringStamped msgs;
+    msgs.header.stamp = ros::Time::now();
+    msgs.header.frame_id = "camera_link";
+    msgs.data = tag_value;
+    pub.publish(msgs);
+  }
+  prev_tag_found = tag_found;
 }
 
 int main(int argc, char **argv){
@@ -104,7 +107,6 @@ int main(int argc, char **argv){
 #endif
   ros::init(argc, argv, node_name);
   ros::NodeHandle n("~");
-  n.param<vector<string> >("known_qr_codes", knownCodes, vector<string>());
   ros::Subscriber sub = n.subscribe("/usb_cam/image_raw",1, camCallback);
   pub = n.advertise<msgs::StringStamped>("/tag", 1);
   scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
