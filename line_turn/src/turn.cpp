5f1cba2c4f59ec187c2930f0a6937449248e2f0c
MortenGundesen <mogun11@student.sdu.dk>
2015-11-04 15:22:32 +0100

    Added a simple turn method, that should work if the quatorion transform is correct

---
+++ line_turn/src/turn.cpp
@@ -0,0 +1,65 @@
+#include <ros/ros.h>
+#include <vector>
+#include <tf/transform_datatypes.h>
+#include <tf/tf.h>
+#include <math.h>
+#include <string>
+
+#include <geometry_msgs/TwistStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <line_detection/line.h>
+
+void lineCb(const line_detection::line &msg);
+void odometryCb(const nav_msgs::Odometry &msg);
+
+line_detection::line line_info;
+nav_msgs::Odometry odometry_info;
+
+std::string lineTopicName;
+std::string odometryTopicName;
+std::string commandPubName;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "line_turn_node");
+    ros::NodeHandle n("~");
+
+    n.param<std::string>("line_topic", lineTopicName, "/line_detector/perception/line");
+    n.param<std::string>("odometry_topic", odometryTopicName, "/odometry/filtered");
+    n.param<std::string>("command_pub", commandPubName, "/fmCommand/cmd_vel");
+
+    ros::Subscriber lineSub = n.subscribe(lineTopicName, 1, lineCb);
+    ros::Subscriber enableSub = n.subscribe(odometryTopicName, 1, odometryCb);
+
+    ros::Publisher commandPub = n.advertise<geometry_msgs::TwistStamped>(commandPubName, 1);
+    ros::Rate rate(20);
+
+    double start_yaw = tf::getYaw(odometry_info.pose.pose.orientation);
+
+    while(ros::ok())
+    {
+        if(abs(start_yaw-tf::getYaw(odometry_info.pose.pose.orientation)) > 1.5)
+        {
+            break;
+        }
+
+        geometry_msgs::TwistStamped::Ptr message;
+        message->twist.angular.z = 1;
+        commandPub.publish(message);
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
+
+void lineCb(const line_detection::line &msg)
+{
+    line_info = msg;
+}
+
+void odometryCb(const nav_msgs::Odometry &msg)
+{
+    odometry_info = msg;
+}
