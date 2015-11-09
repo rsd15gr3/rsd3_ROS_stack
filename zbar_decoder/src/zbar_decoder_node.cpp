fe8ced70ba4859272007da5ea02d2a44acbf0852
Birkehoj <anders.birkehoj@gmail.com>
2015-11-09 21:37:01 +0100

    Decoding service added to zbar_decoder

--- zbar_decoder/src/zbar_decoder_node.cpp
+++ zbar_decoder/src/zbar_decoder_node.cpp
@@ -8,27 +8,27 @@
 #include <sensor_msgs/image_encodings.h>
 #include <ros/ros.h>
 #include <msgs/StringStamped.h>
+#include <msgs/BoolStamped.h>
 #include <sensor_msgs/Image.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <zbar.h>
+#include "zbar_decoder/decode_qr.h"
 
 using namespace std;
 using namespace zbar;
 using namespace cv;
 
 #define ROSCONSOLE_MIN_SEVERITY ROSCONSOLE_SEVERITY_DEBUG //ROSCONSOLE_SEVERITY_INFO, ROSCONSOLE_SEVERITY_DEBUG
-
-sensor_msgs::Image rawImg;
 const string node_name("zbar_decoder_node");
-const string no_tag_id("?");
-ImageScanner scanner;
-string tag_value(no_tag_id);
-bool prev_tag_found;
-// change values to match workcells code
-ros::Publisher pub;
-vector<string> knownCodes;//
 
+const double min_white_area = 70;
+const unsigned min_tag_area = 50;
+const int threshold_value = 200;
+ImageScanner scanner;
+bool prev_tag_found;
+cv_bridge::CvImageConstPtr cv_ptr; // http://docs.ros.org/api/sensor_msgs/html/msg/Image.html
+ros::Publisher pub;
 int debounce_size;
 double scale_factor;
 class BoolDebouncer
@@ -53,20 +53,63 @@ private:
   bool prev_val, debuounced_val;
 };
 BoolDebouncer debouncer(debounce_size);
+bool decode_qr_tag(const cv_bridge::CvImageConstPtr &cv_ptr, string & value);
+void camCallback(const sensor_msgs::Image::ConstPtr& img );
+bool get_qr_id_callback(zbar_decoder::decode_qr::Response &req, zbar_decoder::decode_qr::Response &res);
+unsigned getLargestContourROI(const Mat &im, Mat &mask, double min_area);
+bool imHasQRTag(const Mat &im);
 
-void camCallback(const sensor_msgs::Image::ConstPtr& img )
+int main(int argc, char **argv){
+  // Print debug msgs
+#if ROSCONSOLE_MIN_SEVERITY == ROSCONSOLE_SEVERITY_DEBUG
+  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+     ros::console::notifyLoggerLevelsChanged();
+  }
+#endif
+  ros::init(argc, argv, node_name);
+  ros::NodeHandle n("~");  
+  pub = n.advertise<msgs::BoolStamped>("/tag_found", 1);
+  n.param<double>("tag_scale_factor",scale_factor, 0.5);
+  n.param<int>("debounce_size", debounce_size, 3);
+  if(debounce_size < 1) {
+    debounce_size = 1;
+    ROS_WARN("Debounce size less than 1 so defaults to: %i", debounce_size);
+  }
+  ros::Subscriber sub = n.subscribe("/usb_cam/image_raw",1, camCallback);
+  ros::ServiceServer service = n.advertiseService("get_qr_id", get_qr_id_callback);
+  scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+  ros::spin();
+}
+
+bool imHasQRTag(const Mat &im)
 {
-  // Convert image to opencv
-  cv_bridge::CvImageConstPtr cv_ptr; // http://docs.ros.org/api/sensor_msgs/html/msg/Image.html
-  try
+  Mat binary_im;
+  threshold( im, binary_im, threshold_value, 255, CV_THRESH_BINARY );
+  Mat mask;
+  unsigned roi_size = getLargestContourROI(binary_im, mask, min_white_area);
+  if(roi_size > min_white_area)
   {
-    cv_ptr = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::MONO8);
+    Mat roi;
+    bitwise_not(binary_im, roi, mask);
+    unsigned tag_area = countNonZero(roi);
+    if(tag_area > min_tag_area)
+    {
+      return true;
+    }
   }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+  return false;
+}
+
+bool get_qr_id_callback(zbar_decoder::decode_qr::Response &req, zbar_decoder::decode_qr::Response &res)
+{
+  string qr_tag;
+  bool tag_in_im = decode_qr_tag(cv_ptr, qr_tag);
+  //res.value = 8;
+  res.value = qr_tag;
+}
+
+bool decode_qr_tag(const cv_bridge::CvImageConstPtr &cv_ptr, string & value)
+{
   // extreact qr code
   cv::Mat down_scaled_im;
   cv::resize(cv_ptr->image, down_scaled_im, Size(), scale_factor, scale_factor);
@@ -80,48 +123,70 @@ void camCallback(const sensor_msgs::Image::ConstPtr& img )
   if(n < 0)
   {
     ROS_ERROR_NAMED(node_name,"Error decoding image with Image scan");
+  }
+  else
+  {
+    value = image.symbol_begin()->get_data();
+  }
+  return n > 0;
+}
+
+unsigned getLargestContourROI(const Mat &im, Mat &mask, double min_area)
+{
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+  Mat tmp = im.clone();
+  findContours( tmp, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  double largest_contour_area = min_area;
+  unsigned maxAreaIdx = -1;
+  vector<Point> largest_contour;
+  for( int i = 0; i< contours.size(); i++ )
+  {
+    double area = contourArea(contours[i]);
+    if(area > largest_contour_area) {
+      maxAreaIdx = i;
+      largest_contour_area=area;
+    }
+  }
+  if(maxAreaIdx == -1)
+  {
+    //ROS_DEBUG("No barcode");
+  }
+  else
+  {
+    mask = cv::Mat::zeros(im.size(), im.type());
+    cv::drawContours(mask, contours, maxAreaIdx, cv::Scalar(255),CV_FILLED);
+  }
+  return largest_contour_area;
+}
+
+void camCallback(const sensor_msgs::Image::ConstPtr& img )
+{
+  // Convert image to opencv
+  try
+  {
+    cv_ptr = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
-  bool current_tag_found = n > 0;
+  bool current_tag_found = imHasQRTag(cv_ptr->image);
   bool tag_found = debouncer.update(current_tag_found);
-  if(tag_found) // qr found?
+  if(tag_found && !prev_tag_found) // qr found?
   {
     if(current_tag_found)
     {
       ROS_DEBUG("Tag found");
       // first found tag is used
-      tag_value = image.symbol_begin()->get_data();
+      //ROS_DEBUG_COND(qr_tag_found, "qr tag not found by zbar");
+      msgs::BoolStamped msgs;
+      msgs.header.stamp = ros::Time::now();
+      msgs.header.frame_id = "camera_link";
+      msgs.data = tag_found;
+      pub.publish(msgs);
     }
   }
-  else if(prev_tag_found) // else n==0 -> no code found
-  {
-    ROS_DEBUG("Tag moved out of camera view");
-    msgs::StringStamped msgs;
-    msgs.header.stamp = ros::Time::now();
-    msgs.header.frame_id = "camera_link";
-    msgs.data = tag_value;
-    pub.publish(msgs);
-  }
   prev_tag_found = tag_found;
-}
-
-int main(int argc, char **argv){
-  // Print debug msgs
-#if ROSCONSOLE_MIN_SEVERITY == ROSCONSOLE_SEVERITY_DEBUG
-  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
-     ros::console::notifyLoggerLevelsChanged();
-  }
-#endif
-  ros::init(argc, argv, node_name);
-  ros::NodeHandle n("~");
-  ros::Subscriber sub = n.subscribe("/usb_cam/image_raw",1, camCallback);
-  pub = n.advertise<msgs::StringStamped>("/tag", 1);
-  n.param<double>("tag_scale_factor",scale_factor, 0.5);
-  n.param<int>("debounce_size", debounce_size, 3);
-  if(debounce_size < 1) {
-    debounce_size = 1;
-    ROS_WARN("Debounce size less than 1 so defaults to: %i", debounce_size);
-  }
-  scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
-  ros::spin();
 }
