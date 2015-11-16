47e3ed07ec855b2f0a84b5122c482cc3043dd6b5
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2015-11-16 15:46:29 +0100

    Add amcl_resetter package

---
+++ amcl_resetter/src/amcl_resetter_node.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
+
+
+class AMCLResetterNode:
+    def __init__(self):
+        rospy.init_node("amcl_resetter_node")
+
+        self.dist_limit = 2
+        self.odom_filtered = None
+        self.odom_markerlocator = None
+
+        rospy.Subscriber("odometry/filtered", Odometry, self.odom_filtered_cb)
+        rospy.Subscriber("odometry/markerlocator", Odometry, self.odom_markerlocator_cb)
+
+        self.init_pose_pub = rospy.Publisher("initialpose", PoseWithCovarianceStamped, queue_size=1)
+
+        self.tf = tf.TransformListener()
+
+        self.loop()
+
+    def odom_filtered_cb(self, odom_msg):
+        self.odom_filtered = odom_msg
+
+    def odom_markerlocator_cb(self, odom_msg):
+        self.odom_markerlocator = odom_msg
+
+    def odom_to_posestamped(odom_msg):
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = odom_msg.header.stamp
+        pose_msg.header.frame_id = odom_msg.header.frame_id
+        pose_msg.pose = odom_msg.pose.pose
+        return pose_msg
+
+    def calc_dist_and_publish(self):
+        if self.odom_filtered is None or self.odom_markerlocator is None:
+            return
+
+        try:
+            pose_filtered = tf.transformPose("map", self.odom_to_posestamped(self.odom_filtered))
+            pose_markerlocator = tf.transformPose("map", self.odom_to_posestamped(self.odom_markerlocator))
+        except (tf.LookupException, tf.ConnectivityException):
+            print "fuck"
+            pass
+
+        p1 = np.array((pose_filtered.pose.position.x, pose_filtered.pose.position.y))
+        p2 = np.array((pose_markerlocator.pose.position.x, pose_markerlocator.pose.position.y))
+        dist = np.linalg.norm(p2 - p1)
+        print dist
+
+        if dist < self.dist_limit:
+            initialpose_msg = PoseWithCovarianceStamped()
+            initialpose_msg.header.stamp = rospy.Time.now()
+            initialpose_msg.header.frame_id = "map"
+            initialpose_msg.pose = pose_markerlocator.pose
+            self.init_pose_pub.publish(initialpose_msg)
+
+    def loop(self):
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        node = AMCLResetterNode()
+    except rospy.ROSInterruptException:
+        pass
