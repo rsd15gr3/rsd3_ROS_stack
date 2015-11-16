10e0edd4c007571013c5c4dd28c34ef5e683d529
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2015-11-16 17:04:16 +0100

    Fix odom message topic names

--- amcl_resetter/src/amcl_resetter_node.py
+++ amcl_resetter/src/amcl_resetter_node.py
@@ -18,7 +18,7 @@ class AMCLResetterNode:
         self.tf_listener = tf.TransformListener()
         self.tf_listener.waitForTransform("map", "markerlocator", rospy.Time(0), rospy.Duration(5))
 
-        rospy.Subscriber("odometry/filtered", Odometry, self.odom_filtered_cb)
+        rospy.Subscriber("odometry/filtered/local", Odometry, self.odom_filtered_cb)
         rospy.Subscriber("odometry/markerlocator", Odometry, self.odom_markerlocator_cb)
 
         self.init_pose_pub = rospy.Publisher("initialpose", PoseWithCovarianceStamped, queue_size=1)
