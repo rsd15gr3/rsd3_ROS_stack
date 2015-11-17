81f8af5bb194e6cde39b2c8866849508e343c969
Birkehoj <anders.birkehoj@gmail.com>
2015-11-17 14:11:25 +0100

    Free navigation as action server that approaches targets with move base

--- free_navigation/src/free_navigation.cpp
+++ free_navigation/src/free_navigation.cpp
@@ -1,19 +1,116 @@
-#include <free_navigation/free_navigation.h>
+#include "free_navigation.h"
 #include<boost/bind.hpp>
 #include <tf/tf.h>
 #include <mission/action_states.h>
-Navigation::Navigation()
-    : ac_("move_base", true)
+Navigation::Navigation(std::string name)
+    : name_(name), as_(nh_, name, false),move_base_ac_("move_base", true), base_frame_id_("map")
 {
-    while(!ac_.waitForServer(ros::Duration(5.0)) && ros::ok()){
-        ROS_INFO("Waiting for the move_base action server to come up");
-    }
+  ROS_INFO("Starting navigation");
+  // setup action server
+  as_.registerGoalCallback(boost::bind(&Navigation::goalCb, this) );
+  as_.registerPreemptCallback(boost::bind(&Navigation::preemtCb, this) );
+
+  // setup move base behavior
+  {
+      vector<double> tmp_in_plane_pose;
+      nh_.param<vector<double>>("line_to_manipulator", tmp_in_plane_pose, vector<double>{0,0,0});
+      line_to_manipulator_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
+      nh_.param<vector<double>>("delivery_pose", tmp_in_plane_pose, vector<double>{0,0,0});
+      delivery_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
+      nh_.param<vector<double>>("load_bricks", tmp_in_plane_pose, vector<double>{0,0,0});
+      load_bricks_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
+      nh_.param<vector<double>>("charge", tmp_in_plane_pose, vector<double>{0,0,0});
+      charge_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
+  }
+  //ros::Subscriber sub = n.subscribe(action_topic, 1, &actionCb);
+  while(!move_base_ac_.waitForServer(ros::Duration(5.0)) && ros::ok()){
+      ROS_INFO("Waiting for the move_base action server to come up");
+  }
+  ROS_DEBUG("Starting action server");
+  as_.start();
 }
+
+void Navigation::goalCb()
+{
+  goal_ = as_.acceptNewGoal()->behavior_type;
+  approachGoal();
+  ROS_INFO_NAMED(name_,"New behavior accepted");
+}
+
+void Navigation::preemtCb()
+{
+  as_.setPreempted();
+  ROS_INFO_NAMED(name_,"Behavior preemted");
+  // cancel all actions
+  move_base_ac_.cancelAllGoals();
+}
+
+void Navigation::approachGoal()
+{
+      move_base_ac_.cancelAllGoals();
+      move_base_msgs::MoveBaseGoal goal_msg;
+      switch (goal_) {
+      case CHARGE:
+          ROS_INFO("GOING TO CHARGER: %i", BOX_CHARGE);
+          goal_msg.target_pose.pose = charge_pose_;
+          break;
+      case BRICK:
+          ROS_INFO("GOING TO collect bricks: %i", BRICK);
+          goal_msg.target_pose.pose = load_bricks_pose_;
+          break;
+      case DELIVERY:
+          ROS_INFO("GOING TO DELIVERY: %i", DELIVERY);
+          goal_msg.target_pose.pose = delivery_pose_;
+          break;
+      case TRANSITION:
+          ROS_INFO("GOING TO line to manipulators: %i", TRANSITION);
+          goal_msg.target_pose.pose = line_to_manipulator_pose_;
+          break;
+      default:
+          //ac_.cancelAllGoals();
+          ROS_ERROR_NAMED(name_,"Unknown behavior type: %i",goal_);
+          as_.setAborted(result_, "Unknown behavior type recieved");
+          return; // not a navigation command so do not navigate
+      }
+      ROS_INFO("(%f, %f)",goal_msg.target_pose.pose.position.x, goal_msg.target_pose.pose.position.y);
+      goal_msg.target_pose.header.frame_id = base_frame_id_;
+      goal_msg.target_pose.header.stamp = ros::Time::now();
+      move_base_ac_.sendGoal(goal_msg, boost::bind(&Navigation::doneCb,this,_1, _2),
+                   boost::bind(&Navigation::activeCb, this),
+                   boost::bind(&Navigation::feedbackCb, this, _1) );
+}
+
 
 void Navigation::doneCb(const actionlib::SimpleClientGoalState& state,
                         const move_base_msgs::MoveBaseResultConstPtr& result)
 {
+  if( move_base_ac_.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
+    ROS_ERROR_NAMED(name_, "Move base failed to approach target");
+    as_.setAborted(result_,"Move base failed to approach target");
+  }
   //ROS_INFO("Finished in state: %s", state.getText().c_str());
+  switch (goal_) {
+  case CHARGE:
+      ROS_INFO("Docking in CHARGER: %i", BOX_CHARGE);
+      as_.setSucceeded(result_); // first set succeeded in docking doneCb
+      break;
+  case BRICK:
+      ROS_INFO("going in to collect bricks: %i", BRICK);
+      as_.setSucceeded(result_); // first set succeeded in collecting doneCb
+      break;
+  case DELIVERY:
+      ROS_INFO("Tipping of at DELIVERY: %i", DELIVERY);
+      as_.setSucceeded(result_);
+      break;
+  case TRANSITION:
+      ROS_INFO("At transition: %i", TRANSITION);
+      as_.setSucceeded(result_);
+      break;
+  default:
+      //ac_.cancelAllGoals();
+      return; // not a navigation command so do not navigate
+  }
 }
 void Navigation::activeCb()
 {
@@ -24,60 +121,6 @@ void Navigation::feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& feed
 {
     //ROS_INFO("Feedback update:");
     // Possible to Monitor position on feedback->base_position
-}
-
-void Navigation::actionStateCb(const msgs::IntStamped& action_state)
-{
-    ROS_INFO("Action: %i recieved", action_state.data);
-    if(action_state.data != prev_action_state_)
-    {
-        ac_.cancelAllGoals();
-        move_base_msgs::MoveBaseGoal goal;
-        switch (action_state.data) {
-        case BOX_CHARGE:
-            ROS_INFO("GOING TO CHARGER: %i", BOX_CHARGE);
-            goal.target_pose.pose = charge_pose_;
-            break;
-        case BOX_BRICK:
-            ROS_INFO("GOING TO collect bricks: %i", BOX_CHARGE);
-            goal.target_pose.pose = load_bricks_pose_;
-            break;
-        case GPS_LINE:
-            ROS_INFO("GOING TO line: %i", BOX_CHARGE);
-            goal.target_pose.pose = line_from_manipulator_pose_;
-            break;
-        case GPS_DOOR:
-            ROS_INFO("GOING TO line to manipulators: %i", BOX_CHARGE);
-            goal.target_pose.pose = line_to_manipulator_pose_;
-            ROS_WARN("GPS door is depleted");
-            break;
-        case BOX_DOOR:
-            ROS_WARN("Box door is depleted");
-        default:
-            //ac_.cancelAllGoals();
-            return; // not a navigation command so do not navigate
-        }
-
-    /*
-        Pose testPose;
-        testPose.position.x=3.305;
-        testPose.position.y=0.366;
-        testPose.position.z=0.0;
-        testPose.orientation.x=0.0;
-        testPose.orientation.y=0;
-        testPose.orientation.z=0.9723;
-        testPose.orientation.w=-0.2319;
-         move_base_msgs::MoveBaseGoal goal;
-        goal.target_pose.pose = testPose;
-        */
-        ROS_INFO("(%f, %f)",goal.target_pose.pose.position.x, goal.target_pose.pose.position.y);
-        goal.target_pose.header.frame_id = base_frame_id_;
-        goal.target_pose.header.stamp = ros::Time::now();        
-        ac_.sendGoal(goal, boost::bind(&Navigation::doneCb,this,_1, _2),
-                     boost::bind(&Navigation::activeCb, this),
-                     boost::bind(&Navigation::feedbackCb, this, _1) );
-    }
-    prev_action_state_ = action_state.data;
 }
 
 Pose Navigation::convertVecToPose(const vector<double>& poses)
