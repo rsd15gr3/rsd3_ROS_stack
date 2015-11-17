802d14b6d9ff0e5d6132bf5419915bd25c7d8d1a
Birkehoj <anders.birkehoj@gmail.com>
2015-11-17 14:11:25 +0100

    Free navigation as action server that approaches targets with move base

--- free_navigation/src/free_navigation.h
+++ free_navigation/src/free_navigation.h
@@ -1,10 +1,13 @@
 #ifndef Navigation_H
 #define Navigation_H
+#include <ros/ros.h>
 #include<string>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include<geometry_msgs/Pose.h>
 #include <msgs/IntStamped.h>
+#include <actionlib/server/simple_action_server.h>
+#include <free_navigation/NavigateFreelyAction.h>
 using std::string;
 using std::vector;
 using geometry_msgs::Pose;
@@ -13,23 +16,29 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 class Navigation
 {
 public:
-    Navigation();
-    void setNavigationPoint(int action_state_id, vector<double> in_plane_pose);
-    void actionStateCb(const msgs::IntStamped& action_state);
-    static Pose convertVecToPose(const vector<double>& poses);
+    Navigation(std::string name);
+private:
+    void approachGoal();
+    string name_;
+    ros::NodeHandle nh_;
+
+    // action server
+    actionlib::SimpleActionServer<free_navigation::NavigateFreelyAction> as_;
+    free_navigation::NavigateFreelyResult result_;
+    uint8_t goal_;
+    uint8_t prev_action_state_ = 255;
+    // Move base client
     Pose line_to_manipulator_pose_;
-    Pose line_from_manipulator_pose_;
+    Pose delivery_pose_;
     Pose charge_pose_;
     Pose load_bricks_pose_;
     string base_frame_id_;
-private:
-    MoveBaseClient ac_;
-    int prev_action_state_ = -1;
+    MoveBaseClient move_base_ac_;
     void doneCb(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result);
+    void goalCb();
+    void preemtCb();
     void activeCb();
     void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback);
-
-    void setBaseFrameId(string frame_id);
+    static Pose convertVecToPose(const vector<double>& poses);
 };
-
 #endif // Navigation_H
--- free_navigation/src/free_navigation_node.cpp
+++ free_navigation/src/free_navigation_node.cpp
@@ -2,7 +2,9 @@
 #include <mission/action_states.h>
 #include<msgs/IntStamped.h>
 #include <tf/tf.h>
-#include <free_navigation/free_navigation.h>
+#include "free_navigation.h"
+#define ROSCONSOLE_MIN_SEVERITY ROSCONSOLE_SEVERITY_DEBUG //ROSCONSOLE_SEVERITY_INFO, ROSCONSOLE_SEVERITY_DEBUG
+
 using std::vector;
 using std::string;
 using geometry_msgs::Pose;
@@ -13,28 +15,13 @@ void actionCb(const msgs::IntStamped& cmd)
 }
 
 int main(int argc, char** argv){
+#if ROSCONSOLE_MIN_SEVERITY == ROSCONSOLE_SEVERITY_DEBUG
+  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+     ros::console::notifyLoggerLevelsChanged();
+  }
+#endif
     ros::init(argc,argv,"navigator");
-    ros::NodeHandle n;
-    Navigation navigation;
-    string action_topic;
-    {
-        string base_link_id;
-        n.param<string>("base_link_id", base_link_id, "base_link");
-        navigation.base_frame_id_ = base_link_id;
-        n.param<string>("action_topic", action_topic, "mission/action_state");
-        vector<double> tmp_in_plane_pose;
-        n.param<vector<double>>("line_to_manipulator", tmp_in_plane_pose, vector<double>{0,0,0});
-        navigation.line_to_manipulator_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
-
-        n.param<vector<double>>("line_from_manipulator", tmp_in_plane_pose, vector<double>{0,0,0});
-        navigation.line_from_manipulator_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
-        n.param<vector<double>>("load_bricks", tmp_in_plane_pose, vector<double>{0,0,0});
-        navigation.load_bricks_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
-        n.param<vector<double>>("charge", tmp_in_plane_pose, vector<double>{0,0,0});
-        navigation.charge_pose_ = Navigation::convertVecToPose(tmp_in_plane_pose);
-    }    
-    ros::Subscriber sub = n.subscribe(action_topic, 1, &Navigation::actionStateCb, &navigation);
-    //ros::Subscriber sub = n.subscribe(action_topic, 1, &actionCb);
+    Navigation navigation(ros::this_node::getName());
     ros::spin();
     return 0;
 }
