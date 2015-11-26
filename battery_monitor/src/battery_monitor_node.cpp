fd88738adfb0c95c87428d15a3e993ccae8e3499
Birkehoj <anders.birkehoj@gmail.com>
2015-11-26 19:42:45 +0100

    Bug fixed

--- battery_monitor/src/battery_monitor_node.cpp
+++ battery_monitor/src/battery_monitor_node.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace ros;
 
-double frobit_voltage;
+double frobit_voltage = 0;
 double robocard_voltage_divider = 0.03747; // 5.0*(22000 + 3300)/(3300*1023) because of 22000/3300 ohm v-divider
 
 void nmeaFromFrobitCb(msgs::nmeaConstPtr msg)
@@ -36,19 +36,22 @@ int main(int argc, char **argv)
     nh.param<string>("nmea_from_frobit_sub", nmea_to_frobit_sub, "/fmSignal/nmea_from_frobit");
     nmea_sub = nh.subscribe<msgs::nmeaConstPtr>(nmea_to_frobit_sub, 1,&nmeaFromFrobitCb);
   }
-  Publisher voltage_pub = nh.advertise<msgs::FloatStamped>("~battery_level", 1);
-  Publisher too_low_battery_pub = nh.advertise<msgs::BoolStamped>("~too_low_battery", 1);
+  Publisher voltage_pub = nh.advertise<msgs::FloatStamped>("battery_level", 1);
+  Publisher too_low_battery_pub = nh.advertise<msgs::BoolStamped>("too_low_battery", 1);
   Rate r(publish_rate);
   while (nh.ok()) {
+    ros::spinOnce();
+    r.sleep();
     msgs::FloatStamped battery_level;
     battery_level.header.stamp = Time::now();
     battery_level.data = frobit_voltage;
+    ROS_DEBUG("voltage= %f", frobit_voltage);
     voltage_pub.publish(battery_level);
     msgs::BoolStamped too_low_battery_msgs;
     too_low_battery_msgs.header.stamp = Time::now();
     too_low_battery_msgs.data = frobit_voltage < min_battery_level;
+    ROS_DEBUG("below minimum = %i", frobit_voltage < min_battery_level);
     too_low_battery_pub.publish(too_low_battery_msgs);
-    r.sleep();
   }
   return 0;
 }
