cc30293df746463d456daa902717e9bf1f3e8fec
kitt10 <bulinmartin@gmail.com>
2015-11-30 17:04:29 +0100

    Parameters adjusted. Server done.

--- collect_bricks_pos/setup.py
+++ collect_bricks_pos/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['src/load_pos_node.py'],)
+  scripts=['src/collect_bricks_pos_node.py'],)
 setup(**d)
 
--- collect_bricks_pos/src/collect_bricks_pos_node.py
+++ collect_bricks_pos/src/collect_bricks_pos_node.py
@@ -4,17 +4,19 @@ import rospy
 from msgs.msg import IntStamped
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import LaserScan
-from math import sin, cos
+from collect_bricks_pos.msg import collect_bricks_posAction
 import numpy as np
+import actionlib
 
 
-class Node():
+class LoadPosActionNode():
 
     def __init__(self):
         """ Node Instance Initialization """
 
         ''' Topics '''
         self.tp_scan = '/fmSensors/scan'
+        #self.tp_scan = '/base_scan'
         self.tp_frobit_automode = '/fmPlan/automode'
         self.tp_cmd_vel = '/fmCommand/cmd_vel'
 
@@ -25,15 +27,15 @@ class Node():
         ''' Publishing rate '''
         self.publishing_rate = rospy.Rate(10)          # [Hz]
 
+        ''' Parameters '''
+        # Hardcoded parameters here when optimized
+
         ''' Variables '''
         self.frobit_automode = 0
         self.scan_ranges = list()
-        self.scan_angles = list()
-        self.c_x = list()
-        self.c_y = list()
         self.vel_lin = 0.0
         self.vel_ang = 0.0
-        self.right_wall = 40
+        self.right_wall = 0.4
         self.front_left_wall = 0
         self.front_right_wall = 0
 
@@ -41,21 +43,18 @@ class Node():
         rospy.Subscriber(self.tp_scan, LaserScan, self.on_tp_scan)
         rospy.Subscriber(self.tp_frobit_automode, IntStamped, self.on_tp_frobit_automode)
 
-        # Loop function
-        self.keep_publishing()
+        ''' ActionServer '''
+        self.finished = False
+        self.success = True
+        self.action_name = 'collect_bricks_pos_server'
+        self.action_server = actionlib.SimpleActionServer(self.action_name, collect_bricks_posAction, execute_cb=self.run_action, auto_start=False)
+        self.action_server.start()
 
     def on_tp_scan(self, msg):
         del self.scan_ranges[:]
-        del self.scan_angles[:]
-        del self.c_x[:]
-        del self.c_y[:]
         for i, a_range in enumerate(msg.ranges):
             if msg.range_min < a_range < msg.range_max:
-                angle = msg.angle_min + msg.angle_increment * i
                 self.scan_ranges.append(a_range)
-                self.scan_angles.append(angle)
-                self.c_x.append(cos(angle)*a_range)
-                self.c_y.append(sin(angle)*a_range)
 
     def on_tp_frobit_automode(self, msg):
         self.frobit_automode = msg.data
@@ -75,23 +74,26 @@ class Node():
             except IndexError:
                 pass
 
-        if 0.4 < self.front_left_wall < 0.6 and 0.4 < self.front_right_wall < 0.6:
+        if 0.25 < self.front_left_wall < 0.6 and 0.25 < self.front_right_wall < 0.6:
             print 'front wall seen', self.front_left_wall, self.front_right_wall
-            self.vel_lin = 0.0
-            self.vel_ang = 0.0
-            if abs(self.front_right_wall-self.front_left_wall) > 0.01:
+            self.stop_frobit()
+            if abs(self.front_right_wall-self.front_left_wall) > 0.1:
                 if self.front_right_wall > self.front_left_wall:
                     self.vel_ang = 0.1
                 else:
                     self.vel_ang = -0.1
+            else:
+                self.action_server.set_succeeded()
+                self.finished = True
+                self.stop_frobit()
         else:
             self.vel_lin = 0.15
 
-            if self.right_wall < 0.40:
+            if self.right_wall < 0.45:
                 self.vel_ang = 0.35
                 print 'right wall close, turning left', self.right_wall
-            elif self.right_wall > 0.45:
-                self.vel_ang = -0.2
+            elif self.right_wall > 0.50:
+                self.vel_ang = -0.15
                 print 'right wall far away, turning right', self.right_wall
             else:
                 self.vel_ang = 0.0
@@ -103,8 +105,13 @@ class Node():
         self.vel_ang = 0.0
         self.vel_lin = 0.0
 
-    def keep_publishing(self):
-        while not rospy.is_shutdown():
+    def run_action(self, _goal_):
+        while not self.finished and self.success and not rospy.is_shutdown():
+            if self.action_server.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self.action_name)
+                self.action_server.set_preempted()
+                self.success = False
+                break
             if self.frobit_automode:
                 self.go_to_goal()
             self.publishing_rate.sleep()
@@ -112,8 +119,6 @@ class Node():
 if __name__ == '__main__':
     """ The program starts here by naming the node and initializing it. """
 
-    rospy.init_node('load_pos_node')
-    try:
-        Node()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('collect_bricks_pos_node')
+    LoadPosActionNode()
+    rospy.spin()
